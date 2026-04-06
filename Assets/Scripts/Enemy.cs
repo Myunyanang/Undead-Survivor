2c8a25223cba7722747125733635a332c20886f6
조수아 <>
2026-04-06 20:58:56 +0900

    Vedio 05
    몬스터 추가

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D target;
+    bool isLive = true;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!isLive)
+            return;
+
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.linearVelocity = Vector2.zero;
+    }
+
+    private void LateUpdate()
+    {
+        if (!isLive)
+            return;
+
+        spriter.flipX = transform.position.x > target.position.x;
+    }
+}
--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
     void OnTriggerExit2D(Collider2D collision)
     { 
         if(!collision.CompareTag("Area")) { return; }
@@ -28,6 +34,13 @@ public class Reposition : MonoBehaviour
                     transform.Translate(Vector3.up * dirY * 40);
                 }
                 break;
+            case "Enemy":
+                if(collision.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));
+                }
+                break;
+
         }
     }
 }
